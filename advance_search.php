59e04512b5dc20d2ce905f774a8b258472e8ac23
ghkd2121 <48081388+ghkd2121@users.noreply.github.com>
2019-03-05 16:42:09 +0900

    .phpfile

---
+++ advance_search.php
@@ -0,0 +1,63 @@
+<?php
+ $connect = mysqli_connect("localhost", "root", "ls22d390", "projectlawsa");
+ if(isset($_POST["submit"]))
+ {
+      if(!empty($_POST["search"]))
+      {
+           $query = str_replace(" ", "+", $_POST["search"]);
+           header("location:advance_search.php?search=" . $query);
+      }
+ }
+ ?>
+ <!DOCTYPE html>
+ <html lang="ko" dir="ltr">
+      <head>
+           <title>Project Lawsa</title>
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+      </head>
+      <body>
+           <br /><br />
+           <div class="container" style="width:500px;">
+                <h3 align="center">LAWSA에게 무엇이든 물어보세요!</h3><br />
+                <form method="post">
+                     <label>어떤 일이 있었나요?</label>
+                     <input type="text" name="search" class="form-control" value="<?php if(isset($_GET["search"])) echo $_GET["search"]; ?>" />
+                     <br />
+                     <input type="submit" name="submit" class="btn btn-info" value="Search" />
+                </form>
+                <br /><br />
+                <div class="table-responsive">
+                     <table class="table table-bordered">
+                     <?php
+                     if(isset($_GET["search"]))
+                     {
+                          $condition = '';
+                          $query = explode(" ", $_GET["search"]);
+                          foreach($query as $text)
+                          {
+                               $condition .= "video_title LIKE '%".mysqli_real_escape_string($connect, $text)."%' OR ";
+                          }
+                          $condition = substr($condition, 0, -4);
+                          $sql_query = "SELECT * FROM tbl_video WHERE " . $condition;
+                          $result = mysqli_query($connect, $sql_query);
+                          if(mysqli_num_rows($result) > 0)
+                          {
+                               while($row = mysqli_fetch_array($result))
+                               {
+                                    echo '<tr><td><a href="' . $row["video_link"] . '">' . $row["video_title"] . '</a></td></tr>';
+
+                               }
+                          }
+                          else
+                          {
+                               echo '<label>Data not Found</label>';
+                          }
+                     }
+                     ?>
+                     </table>
+                </div>
+           </div>
+      </body>
+ </html>
